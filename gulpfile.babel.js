256d5b48048c9c8de0f42cfde708c4e4ba3dd5fa
Robert Krieg <hello@robertkrieg.ch>
2016-12-19 17:14:10 +0100

    Improve gulpfile and add packages

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,32 +8,33 @@ import webpackStream  from 'webpack-stream';
 import nodemon from 'gulp-nodemon';
 import babel from 'gulp-babel';
 
-gulp.task('build', ['copy-assets','build-sass','build-client', 'build-server']);
+gulp.task('build', ['build-client', 'build-server']);
 
-gulp.task('build-client', ['concat-vendor'], () =>
+gulp.task('build-client', ['copy-assets', 'build-sass', 'concat-vendor'], () =>
     gulp.src('src/js/client/app.js')
         .pipe(webpackStream(require('./webpack.config.js')))
         .pipe(gulp.dest('public/build/js/'))
 );
 
-gulp.task('build-server', [], () =>
-    gulp.src(['src/js/server/server.js'])
-    .pipe(babel())
-    .pipe(gulp.dest('server/'))
+
+gulp.task('build-server', () =>
+    gulp.src('src/js/server/server.js')
+        .pipe(webpackStream(require('./webpack.server.config.js')))
+        .pipe(gulp.dest('server/'))
 );
 
-gulp.task('build-shared', () =>
-    gulp.src(['src/shared/**/*.*'])
-        .pipe(babel())
-        .pipe(gulp.dest('public/build/js/shared/'))
-);
 
-gulp.task('watch', ['build'], () => {
+gulp.task('watch-client', ['build-client'], () => {
     gulp.watch(['src/scss/**/*.*'], ['build-sass']);
     gulp.watch(['src/js/client/**/*.*'], ['build-client']);
-    gulp.watch(['src/js/server/**/*.*'], ['build-server']);
-    gulp.watch(['src/js/shared/**/*.*'], ['build-server', 'build-client']);
+    gulp.watch(['src/js/shared/**/*.*'], ['build-client']);
     gulp.watch(['src/assets/**/*.*'], ['copy-assets']);
+});
+
+
+gulp.task('watch-server', ['build-server'], () => {
+    gulp.watch(['src/js/server/**/*.*'], ['build-server']);
+    gulp.watch(['src/js/shared/**/*.*'], ['build-server']);
     gulp.start('run');
 });
 
@@ -66,8 +67,9 @@ gulp.task('run', () => {
         script: 'server/server.js',
         // args: ["config.json"],
         ext: 'js',
-        watch: 'src'
+        watch: 'server'
     })
 });
 
-gulp.task('default', ['build', 'run']);
+gulp.task('watch', ['watch-client']);
+gulp.task('default', ['build']);
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,6 @@ module.exports = {
     },
     devtool: 'source-map',
     plugins: [
-        new webpack.optimize.UglifyJsPlugin()
+        //new webpack.optimize.UglifyJsPlugin()
     ]
 };
