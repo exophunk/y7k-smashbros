c42b7cc612b758afd019a1ba2e467de7306e2cc6
Robert Krieg <hello@robertkrieg.ch>
2017-01-13 00:20:09 +0100

    Add Multiroom Server possibility

---
+++ src/js/server/GameRoom.js
@@ -0,0 +1,205 @@
+import Config from 'shared/config/Config';
+import Player from 'shared/objects/Player';
+import {PlayerStates} from 'shared/objects/Player';
+import Throwable from 'shared/objects/Throwable';
+import {ThrowableStates} from 'shared/objects/Throwable';
+import SnapshotHelper from 'shared/util/SnapshotHelper';
+
+const TICK_RATE = 66;
+const UPDATE_RATE = 22;
+
+export default class GameRoom {
+
+
+    constructor(io, roomKey, data) {
+
+        this.io = io;
+        this.roomKey = roomKey;
+        this.data = data;
+
+        this.state = {
+            players: {},
+            throwables: {}
+        };
+
+        this.initThrowables();
+
+        this.lastWorldSnapshot = this.state;
+
+        //this.simulationLoop();
+        this.updateLoop();
+        this.debugLoop();
+    }
+
+
+    initThrowables() {
+
+        this.data.throwablesData.forEach((throwableData) => {
+            let throwable = new Throwable(throwableData.id, throwableData.key);
+            throwable.setPos(throwableData.x, throwableData.y);
+            this.state.throwables[throwable.id] = throwable;
+        })
+
+    }
+
+
+    // ---------------------------------------------------------------------------------------------------------
+    // LOOPS
+
+
+    debugLoop() {
+        // console.log('----------');
+        // Object.keys(this.state.players).forEach((id) => {
+        //     console.log(JSON.stringify(this.state.players[id]));
+        // });
+        this.debugLoopTimeout = setTimeout(this.debugLoop.bind(this), 2000);
+    }
+
+
+    simulationLoop() {
+        this.simulationLoopTimeout = setTimeout(this.simulationLoop.bind(this), 1000 / TICK_RATE);
+    }
+
+
+    updateLoop() {
+
+        let snapshot = this.getWorldDelta();
+        //let snapshot = this.getWorldSnapshot();
+
+        if(snapshot) {
+            snapshot.serverTime = new Date().getTime();
+            this.io.to(this.roomKey).emit('update_world', snapshot);
+        }
+
+        this.updateLoopTimeout = setTimeout(this.updateLoop.bind(this), 1000 / UPDATE_RATE);
+    }
+
+
+    // ---------------------------------------------------------------------------------------------------------
+
+
+    getWorldSnapshot() {
+        return {
+            players: JSON.parse(JSON.stringify(this.state.players)),
+            throwables: JSON.parse(JSON.stringify(this.state.throwables)),
+        };
+    }
+
+    getWorldDelta() {
+        let snapshot = this.getWorldSnapshot();
+        let delta = SnapshotHelper.getObjectDelta(snapshot, this.lastWorldSnapshot, ['id']);
+
+        if(delta.players == null) delta.players = {};
+        if(delta.throwables == null) delta.throwables = {};
+
+        this.lastWorldSnapshot = snapshot;
+        return delta;
+    }
+
+
+    getPlayer(id) {
+        return this.state.players[id];
+    }
+
+
+    getThrowable(id) {
+        return this.state.throwables[id];
+    }
+
+
+    getPlayersCount() {
+        return Object.keys(this.state.players).length;
+    }
+
+
+    joinRoom(socket, player) {
+
+        this.state.players[player.id] = player;
+
+        const confirmData = { id: player.id, worldSnapshot: this.getWorldSnapshot() };
+        socket.emit('confirm_join', confirmData);
+        socket.broadcast.to(this.roomKey).emit('enemy_joined', player.getFullSnapshot());
+
+        socket.on('update_from_client', (updates) => { this.updateFromClient(socket, updates); });
+        socket.on('player_hit', (playerId) => { this.hitPlayer(player.id); });
+        socket.on('disconnect', () => { this.leavePlayer(socket, player.id); });
+        console.log('Client (' + player.id + ') entered Room ' + this.roomKey);
+    }
+
+
+    updateFromClient(socket, clientUpdates) {
+
+        if(clientUpdates.player && this.getPlayer(clientUpdates.player.id)) {
+            this.updatePlayer(clientUpdates.player);
+        } else {
+            console.log('Unknown player????' + clientUpdates.player.id);
+            socket.disconnect('unknown');
+            return;
+        }
+
+        if(clientUpdates.throwable && this.getThrowable(clientUpdates.throwable.id)) {
+            this.updateThrowable(clientUpdates.throwable);
+        }
+
+    }
+
+
+    updatePlayer(updatedPlayer) {
+        let player = this.getPlayer(updatedPlayer.id);
+        player.update(updatedPlayer);
+    }
+
+
+    hitPlayer(playerId) {
+        let player = this.getPlayer(playerId);
+
+        if(player) {
+            player.health--;
+
+            if(player.health < 0) {
+                player.state = PlayerStates.DEAD;
+            } else {
+                player.state = PlayerStates.HIT;
+
+                setTimeout(() => {
+                    player.state = PlayerStates.ALIVE;
+                }, Config.HIT_FREEZE_TIME);
+
+            }
+
+            this.io.to(this.roomKey).emit('player_hit', player.getFullSnapshot());
+
+        }
+
+    }
+
+
+    updateThrowable(updatedThrowable) {
+        let throwable = this.getThrowable(updatedThrowable.id);
+        throwable.update(updatedThrowable);
+    }
+
+
+    resetThrowable(throwable) {
+        throwable.state = ThrowableStates.IDLE;
+        throwable.item.anchor = { x: 0.5, y: 0.5 };
+    }
+
+
+    leavePlayer(socket, playerId) {
+
+        Object.values(this.state.throwables).forEach((throwable) => {
+            if(throwable.carryingPlayerId == playerId) {
+                this.resetThrowable(throwable);
+            }
+        });
+
+        delete this.state.players[playerId];
+        socket.broadcast.to(this.roomKey).emit('enemy_left', playerId);
+        console.log('Player left');
+    }
+
+
+
+
+}
--- src/js/server/server.js
+++ src/js/server/server.js
@@ -7,20 +7,16 @@ import dotenv from 'dotenv';
 import path from 'path';
 import fs from 'fs';
 
-import Config from 'shared/config/Config';
+import GameRoom from 'server/GameRoom';
 import Player from 'shared/objects/Player';
-import {PlayerStates} from 'shared/objects/Player';
-import Throwable from 'shared/objects/Throwable';
-import {ThrowableStates} from 'shared/objects/Throwable';
-import SnapshotHelper from 'shared/util/SnapshotHelper';
 
 dotenv.config();
 
 global.isClient = false;
 global.isServer = true;
 
-const TICK_RATE = 66;
-const UPDATE_RATE = 22;
+const MAX_ROOMS = 10;
+const MAX_ROOM_PLAYERS = 2;
 const THROWABLES_DATA_PATH = 'public/build/assets/data/throwables.json';
 
 export default class Server {
@@ -33,95 +29,15 @@ export default class Server {
         this.port = process.env.WEBSOCKET_PORT || 3000;
         this.app.use(express.static(path.resolve('public')))
         this.server.listen(this.port, () => { this.onListen() });
-
         this.io = new SocketIO(this.server);
+
+        this.gameRooms = [];
+        this.data = {};
+        this.data.throwablesData = JSON.parse(fs.readFileSync(THROWABLES_DATA_PATH, 'utf8'));
+
         this.io.on('connection', (socket) => { this.onConnection(socket) });
 
-        this.sockets = [];
-        this.state = {
-            players: {},
-            throwables: {}
-        };
 
-        this.initThrowables();
-
-        this.lastWorldSnapshot = this.state;
-
-        //this.simulationLoop();
-        this.updateLoop();
-        this.debugLoop();
-    }
-
-
-    initThrowables() {
-
-        let throwablesData = JSON.parse(fs.readFileSync(THROWABLES_DATA_PATH, 'utf8'));
-
-        throwablesData.forEach((throwableData) => {
-            let throwable = new Throwable(throwableData.id, throwableData.key);
-            throwable.setPos(throwableData.x, throwableData.y);
-            this.state.throwables[throwable.id] = throwable;
-        })
-
-    }
-
-
-    // ---------------------------------------------------------------------------------------------------------
-    // LOOPS
-
-
-    debugLoop() {
-        // console.log('----------');
-        // Object.keys(this.state.players).forEach((id) => {
-        //     console.log(JSON.stringify(this.state.players[id]));
-        // });
-        this.debugLoopTimeout = setTimeout(this.debugLoop.bind(this), 2000);
-    }
-
-
-    simulationLoop() {
-        this.simulationLoopTimeout = setTimeout(this.simulationLoop.bind(this), 1000 / TICK_RATE);
-    }
-
-
-    updateLoop() {
-        let snapshot = this.getWorldDelta();
-        //let snapshot = this.getWorldSnapshot();
-
-        if(snapshot) {
-            snapshot.serverTime = new Date().getTime();
-            this.io.emit('update_world', snapshot);
-        }
-
-        this.updateLoopTimeout = setTimeout(this.updateLoop.bind(this), 1000 / UPDATE_RATE);
-    }
-
-
-    // ---------------------------------------------------------------------------------------------------------
-
-
-    getWorldSnapshot() {
-        return {
-            players: JSON.parse(JSON.stringify(this.state.players)),
-            throwables: JSON.parse(JSON.stringify(this.state.throwables)),
-        };
-    }
-
-    getWorldDelta() {
-        let snapshot = this.getWorldSnapshot();
-        let delta = SnapshotHelper.getObjectDelta(snapshot, this.lastWorldSnapshot, ['id']);
-        this.lastWorldSnapshot = snapshot;
-        return delta;
-    }
-
-
-    getPlayer(id) {
-        return this.state.players[id];
-    }
-
-
-    getThrowable(id) {
-        return this.state.throwables[id];
     }
 
 
@@ -130,104 +46,54 @@ export default class Server {
     }
 
 
-    joinPlayer(socket, newPlayer, playerId) {
-        let player = new Player();
-        player.update(newPlayer);
-        player.id = playerId;
-        this.state.players[player.id] = player;
-
-        const data = { id: player.id, worldSnapshot: this.getWorldSnapshot() };
-        socket.emit('confirm_join', data);
-        socket.broadcast.emit('enemy_joined', player.getFullSnapshot());
-        console.log('Player joined (' + player.id + ')');
-    }
-
-
-    updateFromClient(socket, clientUpdates) {
-
-        if(clientUpdates.player && this.getPlayer(clientUpdates.player.id)) {
-            this.updatePlayer(clientUpdates.player);
-        } else {
-            console.log('Unknown player????' + clientUpdates.player.id);
-            socket.disconnect('unknown');
-            return;
-        }
-
-        if(clientUpdates.throwable && this.getThrowable(clientUpdates.throwable.id)) {
-            this.updateThrowable(clientUpdates.throwable);
-        }
-
-    }
-
-
-    updatePlayer(updatedPlayer) {
-        let player = this.getPlayer(updatedPlayer.id);
-        player.update(updatedPlayer);
-    }
-
-
-    hitPlayer(playerId) {
-        let player = this.getPlayer(playerId);
-
-        if(player) {
-            player.health--;
-
-            if(player.health < 0) {
-                player.state = PlayerStates.DEAD;
-            } else {
-                player.state = PlayerStates.HIT;
-
-                setTimeout(() => {
-                    player.state = PlayerStates.ALIVE;
-                }, Config.HIT_FREEZE_TIME);
-
-            }
-
-            this.io.emit('player_hit', player.getFullSnapshot());
-
-        }
-
-    }
-
-
-    updateThrowable(updatedThrowable) {
-        let throwable = this.getThrowable(updatedThrowable.id);
-        throwable.update(updatedThrowable);
-    }
-
-
-    resetThrowable(throwable) {
-        throwable.state = ThrowableStates.IDLE;
-        throwable.item.anchor = { x: 0.5, y: 0.5 };
-    }
-
-
-    leavePlayer(socket, playerId) {
-
-        Object.values(this.state.throwables).forEach((throwable) => {
-            if(throwable.carryingPlayerId == playerId) {
-                this.resetThrowable(throwable);
-            }
-        });
-
-        delete this.state.players[playerId];
-        delete this.sockets[socket.id];
-        socket.broadcast.emit('enemy_left', playerId);
-        console.log('Player left');
-    }
-
-
     onConnection(socket) {
-        console.log('new connection');
+        socket.on('join', (playerData) => { this.joinPlayer(socket, playerData); });
+        socket.on('disconnect', () => { this.closeEmptyRooms(); });
+    }
+
+
+    joinPlayer(socket, playerData) {
 
         let playerId = new Date().getTime();
-        this.sockets[socket.id] = socket;
-        socket.on('join', (player) => { this.joinPlayer(socket, player, playerId); });
-        socket.on('update_from_client', (updates) => { this.updateFromClient(socket, updates); });
-        socket.on('player_hit', (playerId) => { this.hitPlayer(playerId); });
-        socket.on('disconnect', () => { this.leavePlayer(socket, playerId); });
+        let player = new Player();
+        player.update(playerData);
+        player.id = playerId;
+
+        let gameRoom = this.getGameRoom();
+        gameRoom.joinRoom(socket, player);
+        socket.join(gameRoom.roomKey);
     }
 
+
+    getGameRoom() {
+        let room = null;
+        for(let gameRoom of this.gameRooms) {
+            if(gameRoom.getPlayersCount() < MAX_ROOM_PLAYERS) {
+                room = gameRoom;
+                break;
+            }
+        };
+
+        if(!room) {
+            let roomKey = 'Room ' + (this.gameRooms.length + 1);
+            room = new GameRoom(this.io, roomKey, this.data);
+            this.gameRooms.push(room);
+            console.log('New Room created! (' + roomKey + ') Currently: ' + this.gameRooms.length + ' open rooms');
+        }
+
+        return room;
+    }
+
+
+    closeEmptyRooms() {
+        for(let gameRoom of this.gameRooms) {
+            if(gameRoom.getPlayersCount() == 0) {
+                console.log('Closing empty room (' + gameRoom.roomKey + ') Currently: ' + this.gameRooms.length + ' open rooms');
+                this.gameRooms.splice(this.gameRooms.indexOf(gameRoom), 1);
+            }
+        };
+
+    }
 }
 
 const server = new Server();
