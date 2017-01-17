577a3b6478899ab4ede2a5e2a5eaba1fc4ac193e
Robert Krieg <hello@robertkrieg.ch>
2017-01-17 21:31:48 +0100

    Improve gulp task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -24,7 +24,7 @@ gulp.task('build-prod', () => {
     gulp.start('build-server')
 })
 
-gulp.task('build-client', ['concat-vendor', 'copy-assets', 'build-sass'], () => {
+gulp.task('build-client', ['prepare-build'], () => {
     let webpackConfig = production ? require('./webpack.client.production.config.js') : require('./webpack.client.config.js')
     webpackConfig.watch = false
     gulp.src('src/js/client/app.js')
@@ -33,30 +33,24 @@ gulp.task('build-client', ['concat-vendor', 'copy-assets', 'build-sass'], () => 
 })
 
 
-gulp.task('build-server', () =>
+gulp.task('build-server', ['copy-admin-templates'], () =>
     gulp.src('src/js/server/server.js')
         .pipe(webpackStream(require('./webpack.server.config.js')))
         .pipe(gulp.dest('server/'))
 )
 
 
-gulp.task('watch', ['build-server'], () => {
-    gulp.watch(['src/scss/**/*.*'], ['build-sass'])
-    gulp.watch(['src/assets/**/*.*'], ['copy-assets'])
-    gulp.watch(['src/js/server/**/*.*', 'src/js/server/**/*.*'], ['build-server'])
-    gulp.start('run-server')
 
-    let webpackConfig = require('./webpack.client.config.js')
-    gulp.src('src/js/client/app.js')
-        .pipe(webpackStream(webpackConfig))
-        .pipe(gulp.dest('public/build/js/'))
-
-})
 
 
 gulp.task('copy-assets', () =>
     gulp.src(['src/assets/**/*.*'])
     .pipe(gulp.dest('public/build/assets'))
+)
+
+gulp.task('copy-admin-templates', () =>
+    gulp.src(['src/views/admin/**/*.*'])
+    .pipe(gulp.dest('server/admin/views'))
 )
 
 gulp.task('concat-vendor', () => {
@@ -86,6 +80,12 @@ gulp.task('build-sass', function () {
         .pipe(sass().on('error', sass.logError))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('public/build/css'))
+
+    gulp.src('src/scss/admin.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('public/build/css'))
 })
 
 
@@ -96,6 +96,25 @@ gulp.task('run-server', () => {
         ext: 'js',
         watch: 'server'
     })
+})
+
+
+gulp.task('prepare-build', ['concat-vendor', 'copy-assets', 'build-sass', 'build-server'], () => {
+
+})
+
+gulp.task('watch', ['prepare-build'], () => {
+    gulp.watch(['src/scss/**/*.*'], ['build-sass'])
+    gulp.watch(['src/assets/**/*.*'], ['copy-assets'])
+    gulp.watch(['src/js/server/**/*'], ['build-server'])
+    gulp.watch(['src/views/**/*.*'], ['copy-admin-templates'])
+    gulp.start('run-server')
+
+    let webpackConfig = require('./webpack.client.config.js')
+    gulp.src('src/js/client/app.js')
+        .pipe(webpackStream(webpackConfig))
+        .pipe(gulp.dest('public/build/js/'))
+
 })
 
 
